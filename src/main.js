43fb9621c7b48a9fd9ced7a0632a1f8131adac2d
leonguyen <nam85it@gmail.com>
2026-04-27 17:05:26 +0700

    update main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import grapesjs from 'grapesjs';
+import 'grapesjs/dist/css/grapes.min.css';
+
+grapesjs.init({
+  container: '#gjs',
+  height: '100vh',
+  fromElement: false,
+  storageManager: false,
+});
